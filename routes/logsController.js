bd689ca445fb067738d2121b926000ca12840b1e
Evan Riegel <eriegz@gmail.com>
2024-10-11 22:51:34 -0600

    File read operations are now asynchronous, and files are read in 20 KB chunks for increased performance

--- routes/logsController.js
+++ routes/logsController.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-const fs = require("fs");
+const fs = require("fs").promises;
 const path = require("path");
 
 const { LOG_DIR } = require("../config");
@@ -13,79 +13,119 @@ router.get("/logs", async (req, res) => {
     : 5;
 
   try {
-    let result = [];
+    let finalOutput = [];
 
-    // If `file` is undefined, process all files in the log directory:
+    // If `file` is undefined, then process all files in the log directory:
     if (!file) {
-      const directoryContents = await fs.promises.readdir(LOG_DIR);
-      const files = directoryContents.filter(it => fs.lstatSync(path.join(LOG_DIR, it)).isFile());
+      const directoryContents = await fs.readdir(LOG_DIR);
 
-      for (const logFile of files) {
-        const filePath = path.join(LOG_DIR, logFile);
-        const lines = await readLinesFromEndOfFile(filePath, numLines, search);
-        result.push({
-          filename: logFile,
-          lines
-        });
-      }
+      finalOutput = await Promise.all(
+        directoryContents.map(async (item) => {
+          const itemPath = path.join(LOG_DIR, item);
+          const itemStats = await fs.lstat(itemPath);
+          if (itemStats.isFile()) {
+            const lines = await readLinesFromEndOfFile(itemPath, numLines, search);
+            return {
+              filename: item,
+              numLines: lines.length,
+              lines
+            };
+          }
+        })
+      );
+
+      // Filter out undefined results from non-file entries
+      finalOutput = finalOutput.filter(Boolean);
     } else {
-      if (!/^[a-zA-Z0-9_.-]+\.([a-zA-Z0-9]+)$/.test(file)) {
+      if (!/^[a-zA-Z0-9_.-]+(\.[a-zA-Z0-9]+)?$/.test(file)) {
         return res
           .status(500)
-          .send({ error: "File names can only include letters, numbers, and single periods" });
+          .send({ error: "File names can only include letters, numbers, dashes, underscores, and single periods" });
       }
 
-      const filePath = path.join(LOG_DIR, file);
-      if (!fs.existsSync(filePath)) {
+      const itemPath = path.join(LOG_DIR, file);
+      try {
+        await fs.access(itemPath);
+      } catch (err) {
+        return res.status(404).send({ error: "Log file not found" });
+      }
+      const itemStats = await fs.lstat(itemPath);
+      if (!itemStats.isFile()) {
         return res.status(404).send({ error: "Log file not found" });
       }
 
-      const lines = await readLinesFromEndOfFile(filePath, numLines, search);
-      result.push({
+      const lines = await readLinesFromEndOfFile(itemPath, numLines, search);
+      finalOutput.push({
         filename: file,
+        numLines: lines.length,
         lines
       });
     }
 
-    res.send(result);
+    res.send(finalOutput);
   } catch (error) {
     console.error(error);
     res.status(500).send({ error: "Error reading log files" });
   }
 });
 
-async function readLinesFromEndOfFile(filePath, n, searchTerm) {
-  logger.info("Beginning line read from file", filePath);
-  const fileSize = fs.statSync(filePath).size;
-  const fileDescriptor = fs.openSync(filePath, 'r');
+async function readLinesFromEndOfFile(filePath, n, search) {
+  const fileSize = (await fs.stat(filePath)).size;
+  const fileDescriptor = await fs.open(filePath, 'r');
 
-  let buffer = Buffer.alloc(1); // Buffer into which we'll read one byte at a time
-  let position = fileSize - 1; // Start from the last byte
+  // 20 KB is the optimum chunk size based on benchmark tests performed on a 156 MB sample log file.
+  // Increasing it beyond 20 KB results in only marginal performance improvements, if any.
+  const CHUNK_SIZE_BYTES = 1024 * 20;
+  let buffer = Buffer.alloc(CHUNK_SIZE_BYTES);
+  let position = fileSize - CHUNK_SIZE_BYTES;
   let chunk = '';
   let outputLines = [];
 
   try {
-    while (position >= 0 && outputLines.length < n) {
-      fs.readSync(fileDescriptor, buffer, 0, 1, position); // Read 1 byte from the current position
-      const character = buffer.toString('utf-8');
-      chunk = character + chunk;
+    while (position >= -CHUNK_SIZE_BYTES && outputLines.length < n) {
+      const bytesToRead = position < 0
+        ? CHUNK_SIZE_BYTES + position
+        : CHUNK_SIZE_BYTES;
+      const positionClamped = Math.max(position, 0);
+      const { bytesRead } = await fileDescriptor.read(buffer, 0, bytesToRead, positionClamped);
 
-      if ((character === '\n' || position == 0) && chunk.length > 1) {
-        if (searchTerm === undefined || chunk.toLowerCase().includes(searchTerm.toLowerCase())) {
-          outputLines.unshift(chunk.trim());
-        }
-        chunk = '';
+      if (bytesRead === 0) {
+        break;
       }
 
-      position--;
+      const data = buffer.toString('utf-8', 0, bytesRead);
+      chunk = data + chunk;
+
+      const lines = chunk
+        .split('\n')
+        .filter(ea => ea.length > 0);
+      chunk = lines.shift(); // Keep the last incomplete line for the next chunk
+
+      // Read the lines starting from the back and check for the search term in each one:
+      for (let i = lines.length - 1; i >= 0; i--) {
+        const line = lines[i].trim();
+        if (search === undefined || line.toLowerCase().includes(search.toLowerCase())) {
+          outputLines.unshift(line);
+        }
+        if (outputLines.length >= n) {
+          break;
+        }
+      }
+
+      position -= CHUNK_SIZE_BYTES;
+    }
+
+    // If the last chunk contained a partial line, process it
+    const match = search === undefined || chunk.toLowerCase().includes(search.toLowerCase());
+    if (chunk.length > 0 && outputLines.length < n && match) {
+      outputLines.unshift(chunk.trim());
     }
   } catch (err) {
     console.error('Error reading file:', err);
   } finally {
-    fs.closeSync(fileDescriptor);
+    await fileDescriptor.close(); // Asynchronously close the file
   }
 
-  logger.info("Done reading from file", filePath);
   return outputLines;
 }
 
